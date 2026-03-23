facca9815d1081248de22fdf9fc67c658f1bdf5d
Lázaro Dutra <dutralazaro@gmail.com>
2026-03-23 23:33:53 +0000

    feat(docs): add routes and pages for new primitives (alert, checkbox, textarea, separator, card, tooltip, dialog, accordion, tabs, select, switch)

--- apps/docs/src/app/app.routes.ts
+++ apps/docs/src/app/app.routes.ts
@@ -1,7 +1,32 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'badge', pathMatch: 'full' },
+  { path: '', redirectTo: 'alert', pathMatch: 'full' },
+  {
+    path: 'alert',
+    loadComponent: () =>
+      import('./pages/alert/alert-page.component').then(m => m.AlertPageComponent),
+  },
+  {
+    path: 'checkbox',
+    loadComponent: () =>
+      import('./pages/checkbox/checkbox-page.component').then(m => m.CheckboxPageComponent),
+  },
+  {
+    path: 'textarea',
+    loadComponent: () =>
+      import('./pages/textarea/textarea-page.component').then(m => m.TextareaPageComponent),
+  },
+  {
+    path: 'separator',
+    loadComponent: () =>
+      import('./pages/separator/separator-page.component').then(m => m.SeparatorPageComponent),
+  },
+  {
+    path: 'card',
+    loadComponent: () =>
+      import('./pages/card/card-page.component').then(m => m.CardPageComponent),
+  },
   {
     path: 'badge',
     loadComponent: () =>
@@ -16,5 +41,35 @@ export const routes: Routes = [
     path: 'input',
     loadComponent: () =>
       import('./pages/input/input-page.component').then(m => m.InputPageComponent),
+  },
+  {
+    path: 'tooltip',
+    loadComponent: () =>
+      import('./pages/tooltip/tooltip-page.component').then(m => m.TooltipPageComponent),
+  },
+  {
+    path: 'dialog',
+    loadComponent: () =>
+      import('./pages/dialog/dialog-page.component').then(m => m.DialogPageComponent),
+  },
+  {
+    path: 'accordion',
+    loadComponent: () =>
+      import('./pages/accordion/accordion-page.component').then(m => m.AccordionPageComponent),
+  },
+  {
+    path: 'tabs',
+    loadComponent: () =>
+      import('./pages/tabs/tabs-page.component').then(m => m.TabsPageComponent),
+  },
+  {
+    path: 'select',
+    loadComponent: () =>
+      import('./pages/select/select-page.component').then(m => m.SelectPageComponent),
+  },
+  {
+    path: 'switch',
+    loadComponent: () =>
+      import('./pages/switch/switch-page.component').then(m => m.SwitchPageComponent),
   },
 ];
