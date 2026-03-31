cc23c48d600b6a028c5645ca3d6968d4dee1fecf
Lázaro Dutra <dutralazaro@gmail.com>
2026-03-31 02:37:46 +0100

    refactor(date-picker): remove unused `MONTHS` constant from components

--- apps/docs/src/components/date-picker/date-picker.component.ts
+++ apps/docs/src/components/date-picker/date-picker.component.ts
@@ -32,10 +32,6 @@ export interface NgAtomsCalendarDay {
 
 export type NgAtomsDatePickerView = 'days' | 'months' | 'years';
 
-const MONTHS = [
-  'January', 'February', 'March', 'April', 'May', 'June',
-  'July', 'August', 'September', 'October', 'November', 'December',
-];
 
 const MONTHS_SHORT = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
